af0e9efa8bc1afee0aa63034b7135fc6d6fa08d1
td4b <edwin.twest@gmail.com>
2019-04-14 13:14:25 -0700

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,6 +2,6 @@
 apt-get -y update
 apt-get -y install python3
 sudo apt -y install awscli
-aws s3 cp s3://${bucket_name}/install.sh .
+aws s3 cp s3://${bucket_name}/${key} .
 chmod +x install.sh
 ./install.sh
