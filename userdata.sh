ffca0d341c77bf94b3e054f84ec315a35c688f09
td4b <edwin.twest@gmail.com>
2019-04-21 11:57:10 -0700

    Added Kolide Fleet Dependancy Startup.

--- userdata.sh
+++ userdata.sh
@@ -9,3 +9,9 @@ chmod +x ${k3s} & chmod +x ${osquery}
 dpkg -i ${osquery}
 mv k3s /home/ubuntu
 cd /home/ubuntu
+wget https://dl.kolide.co/bin/fleet_latest.zip
+unzip fleet_latest.zip 'linux/*' -d fleet
+sudo cp fleet/linux/fleet /usr/bin/fleet
+sudo cp fleet/linux/fleetctl /usr/bin/fleetctl
+sudo apt-get install mysql-server -y
+sudo apt-get install redis-server -y
